92fd9ce57c1ab7877e616443393de2c4a4889f90
Harwin14 <agungharwinn@gmail.com>
2022-10-31 17:41:44 +0700

    searching date aman

--- jquery/routes/users.js
+++ jquery/routes/users.js
@@ -6,54 +6,52 @@ module.exports = function (db) {
   const collection = db.collection('breads');
 
   router.get('/', async (req, res, next) => {
-      try {
-        //searching
-        const{ string, integer, float, date, boolean} = req.query
-        const params = {}
-        if (string) {
-          params['string'] = new RegExp(string, 'i') 
-        }
-        if (integer) {
-          params['integer'] = parseInt(integer) 
-        }
-        if (float) {
-          params['float'] = JSON.parse(float) 
-        }
-        if (date) {
-          params['date'] = date 
-        }
-        // if (req.query.startDate && req.query.endDate) {
-        //   wheres['date'] = { $gte: new Date(`${req.query.startDate}`), $lte: new Date(`${req.query.endDate}`) }
-        // }
-        // else if (req.query.startDate) {
-        //   wheres['date'] = { $gte: new Date(`${req.query.startDate}`) }
-        // }
-        // else if (req.query.endDate) {
-        //   wheres['date'] = { $lte: new Date(`${req.query.endDate}`) }
-        // }
-        if (boolean) {
-          params['boolean'] = boolean 
-        }
-        console.log(params)
-
-        const page = req.query.page || 1
-        const limit = 3
-        const offset = (page - 1) * limit
-        const total = await collection.countDocuments(params)
-        const pages = Math.ceil(total / limit) 
-        const findResult = await collection.find(params).limit(limit).skip(offset).toArray()
-        res.status(200).json({
-          data: findResult,
-          page: parseInt(page),
-          pages: pages,
-          offset
-
-        })
-      } catch (e) {
-        console.log(e)
-        res.json(e)
+    try {
+      //searching
+      const { string, integer, float, startDate, endDate, boolean } = req.query
+      const params = {}
+      if (string) {
+        params['string'] = new RegExp(string, 'i')
       }
-    })
+      if (integer) {
+        params['integer'] = parseInt(integer)
+      }
+      if (float) {
+        params['float'] = JSON.parse(float)
+      }
+
+    
+      if (startDate && endDate) {
+        params['date'] = { $gte: new Date(startDate), $lte: new Date(endDate) }
+      }else if (startDate) {
+        params['date'] = { $gte: new Date(startDate) }
+      }else if (endDate) {
+        params['date'] = { $lte: new Date(endDate) }
+      }
+      
+      if (boolean) {
+        params['boolean'] = boolean
+      }
+      console.log(params)
+
+      const page = req.query.page || 1
+      const limit = 3
+      const offset = (page - 1) * limit
+      const total = await collection.countDocuments(params)
+      const pages = Math.ceil(total / limit)
+      const findResult = await collection.find(params).limit(limit).skip(offset).toArray()
+      res.status(200).json({
+        data: findResult,
+        page: parseInt(page),
+        pages: pages,
+        offset
+
+      })
+    } catch (e) {
+      console.log(e)
+      res.json(e)
+    }
+  })
   //CREATE
   router.post('/', async function (req, res, next) {
     try {                                              //string: req.body.string, integer: parseInt(req.body.integer), float: JSON.parse(req.body.float), date: new Date(req.body.date), boolean: req.body.boolean
